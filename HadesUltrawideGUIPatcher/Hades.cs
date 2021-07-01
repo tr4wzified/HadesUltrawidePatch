316e94634ed58b6e61c0ce5d6e4c70336f9a174b
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-07-01 21:35:48 +0200

    initial commit - Vignette still needs fixing

---
+++ HadesUltrawideGUIPatcher/Hades.cs
@@ -0,0 +1,115 @@
+﻿using GameFinder.StoreHandlers.Steam;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HadesUltrawideGUIPatcher
+{
+    public class Hades
+    {
+        public Hades()
+        {
+            var steamHandler = new SteamHandler();
+            steamHandler.FindAllGames();
+            Game = steamHandler.Games.Find(x => x.Name == "Hades");
+        }
+
+        public void PatchForUltrawide()
+        {
+            var guiPath = Path.Combine(Game.Path, "Content/Game/GUI");
+            var files = Directory.GetFiles(guiPath);
+            foreach (var file in files)
+            {
+                var stringBuilder = new StringBuilder();
+                try
+                {
+                    using (StreamReader sr = new StreamReader(file))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line.Trim().StartsWith("X = "))// || line.Trim(' ').StartsWith("SpacingX = "))
+                            {
+                                var splitSentence = line.Split('=');
+                                int.TryParse(splitSentence[1], out int num);
+                                num = 1290 + (num - 960);
+                                splitSentence[1] = num.ToString();
+                                line = string.Join("= ", splitSentence);
+                            }
+
+                            else if (line.Trim().StartsWith("Width = "))
+                            {
+                                var splitSentence = line.Split('=');
+                                int.TryParse(splitSentence[1], out int num);
+
+                                if (num == 1920)
+                                {
+                                    num = 2580;
+                                    splitSentence[1] = num.ToString();
+                                    line = string.Join("= ", splitSentence);
+                                }
+                            }
+                            stringBuilder.AppendLine(line);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"File {file} could not be read!");
+                    Console.WriteLine(e.ToString());
+                }
+
+                File.WriteAllText(file, stringBuilder.ToString());
+            }
+
+            var sb = new StringBuilder();
+            var uiDataPath = Path.Combine(Game.Path, "Content/Scripts/UIData.lua");
+            try
+            {
+                using (StreamReader sr = new StreamReader(uiDataPath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Trim().StartsWith("X = "))
+                        {
+                            if (line.EndsWith(','))
+                            {
+                                var lineToModify = line.Remove(line.Length - 1);
+                                var splitSentence = lineToModify.Split('=');
+                                int.TryParse(splitSentence[1], out int num);
+                                if (num == 960)
+                                {
+                                    splitSentence[1] = (1290 + (num - 960)).ToString();
+                                    var newLine = string.Join("= ", splitSentence);
+                                    newLine += ',';
+                                    line = newLine;
+                                }
+
+                            }
+                        }
+                        else if (line.StartsWith("ScreenCenterX"))
+                        {
+                            line = "ScreenCenterX = 2580/2";
+                        }
+                        else if (line.StartsWith("ScreenWidth"))
+                        {
+                            line = "ScreenWidth = 2580";
+                        }
+                        sb.AppendLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"File {uiDataPath} could not be read!");
+                Console.WriteLine(e.ToString());
+            }
+            File.WriteAllText(uiDataPath, sb.ToString());
+        }
+        public SteamGame Game { get; }
+    }
+}
---
+++ HadesUltrawideGUIPatcher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GameFinder;
+using GameFinder.StoreHandlers.Steam;
+
+namespace HadesUltrawideGUIPatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hades = new Hades();
+            hades.PatchForUltrawide();
+        }
+    }
+}
