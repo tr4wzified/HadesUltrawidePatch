5237e3bb98fb400a0405cd1da23f6d5228480c86
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-07-02 18:09:56 +0200

    Fix right-aligned items being half-centered

--- HadesUltrawideGUIPatcher/Hades.cs
+++ HadesUltrawideGUIPatcher/Hades.cs
@@ -19,6 +19,14 @@ namespace HadesUltrawideGUIPatcher
 
         public void PatchForUltrawide()
         {
+            PatchGUIConfigs();
+            PatchUIData();
+        }
+
+        private void PatchGUIConfigs()
+        {
+            int alignmentThreshold = 120;
+
             var guiPath = Path.Combine(Game.Path, "Content/Game/GUI");
             var files = Directory.GetFiles(guiPath);
             foreach (var file in files)
@@ -35,7 +43,18 @@ namespace HadesUltrawideGUIPatcher
                             {
                                 var splitSentence = line.Split('=');
                                 int.TryParse(splitSentence[1], out int num);
-                                num = 1290 + (num - 960);
+                                // Right aligned items
+                                if (num >= 1920 - alignmentThreshold)
+                                {
+                                    num = 2580 + (num - 1920);
+                                }
+                                // Center aligned items
+                                else if (num >= alignmentThreshold)
+                                {
+                                    num = 1290 + (num - 960);
+                                }
+                                // The rest is left aligned, not adjusting those
+
                                 splitSentence[1] = num.ToString();
                                 line = string.Join("= ", splitSentence);
                             }
@@ -64,7 +83,10 @@ namespace HadesUltrawideGUIPatcher
 
                 File.WriteAllText(file, stringBuilder.ToString());
             }
+        }
 
+        private void PatchUIData()
+        {
             var sb = new StringBuilder();
             var uiDataPath = Path.Combine(Game.Path, "Content/Scripts/UIData.lua");
             try
@@ -110,6 +132,7 @@ namespace HadesUltrawideGUIPatcher
             }
             File.WriteAllText(uiDataPath, sb.ToString());
         }
+
         public SteamGame Game { get; }
     }
 }
