840af0a3db91cb336d78032a29aa79caf420073d
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-07-05 23:27:52 +0200

    Hugely simplify patching thus far, still need to patch exceptions to the rule

--- HadesUltrawideGUIPatcher/Hades.cs
+++ HadesUltrawideGUIPatcher/Hades.cs
@@ -27,12 +27,6 @@ namespace HadesUltrawideGUIPatcher
         public void PatchForUltrawide()
         {
             PatchGUIConfigs();
-            PatchScripts();
-            PatchAnimations();
-            PatchDebugKeyScreen();
-            PatchAsphodel();
-            PatchObstacles();
-            // Patch functions below touch same files as PatchScripts()
             PatchCreateScreenObstacle();
             PatchVignette();
         }
@@ -100,259 +94,6 @@ namespace HadesUltrawideGUIPatcher
             }
         }
 
-        private void PatchScripts()
-        {
-            var sb = new StringBuilder();
-            var uiDataPath = Path.Combine(Game.Path, "Content/Scripts/UIData.lua");
-            try
-            {
-                using (StreamReader sr = new StreamReader(uiDataPath))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        if (line.Trim().StartsWith("X = "))
-                        {
-                            if (line.EndsWith(','))
-                            {
-                                var lineToModify = line.Remove(line.Length - 1);
-                                var splitSentence = lineToModify.Split('=');
-                                int.TryParse(splitSentence[1], out int num);
-                                if (num == OriginalCenterX)
-                                {
-                                    splitSentence[1] = (NewCenterX + (num - OriginalCenterX)).ToString();
-                                    var newLine = string.Join("= ", splitSentence);
-                                    newLine += ',';
-                                    line = newLine;
-                                }
-
-                            }
-                        }
-                        else if (line.StartsWith("ScreenCenterX"))
-                        {
-                            line = $"ScreenCenterX = {NewInternalWidth}/2";
-                        }
-                        else if (line.StartsWith("ScreenWidth"))
-                        {
-                            line = $"ScreenWidth = {NewInternalWidth}";
-                        }
-                        sb.AppendLine(line);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"File {uiDataPath} could not be read!");
-                Console.WriteLine(e.ToString());
-            }
-            File.WriteAllText(uiDataPath, sb.ToString());
-
-            sb.Clear();
-            var roomPresentationPath = Path.Combine(Game.Path, "Content/Scripts/RoomPresentation.lua");
-            try
-            {
-                using (StreamReader sr = new StreamReader(roomPresentationPath))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        if (line.Trim().StartsWith("X = "))
-                        {
-                            var splitSentence = line.Split('=');
-                            if (int.TryParse(splitSentence[1], out int num))
-                            {
-                                line = line.Remove(line.Length - 1);
-                                // Right aligned items
-                                if (num >= OriginalInternalWidth - SideAlignmentThreshold)
-                                {
-                                    num = Convert.ToInt32(NewInternalWidth) + (num - OriginalInternalWidth);
-                                }
-                                // Center aligned items
-                                else if (num >= SideAlignmentThreshold)
-                                {
-                                    num = Convert.ToInt32(NewCenterX) + (num - OriginalCenterX);
-                                }
-                                // The rest is left aligned, not adjusting those
-
-                                splitSentence[1] = num.ToString();
-                                var splitSentenceString = string.Join("= ", splitSentence);
-                                splitSentenceString += ',';
-                                line = splitSentenceString;
-                            }
-                        }
-
-                        sb.AppendLine(line);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"File {roomPresentationPath} could not be read!");
-                Console.WriteLine(e.ToString());
-            }
-
-            File.WriteAllText(roomPresentationPath, sb.ToString());
-
-            sb.Clear();
-            var traitTrayScriptsPath = Path.Combine(Game.Path, "Content/Scripts/TraitTrayScripts.lua");
-            try
-            {
-                using (StreamReader sr = new StreamReader(traitTrayScriptsPath))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        if (line.Contains("ScreenAnchors.TraitTrayScreen.BackingDecor = CreateScreenObstacle({ Name = cosmeticData.TraitBackingDecor, Group = \"Combat_Menu_TraitTray_Backing\", X = 960, Y = 1015 })"))
-                        {
-                            line = line.Replace("960", NewCenterX.ToString());
-                        }
-                        sb.AppendLine(line);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"File {roomPresentationPath} could not be read!");
-                Console.WriteLine(e.ToString());
-            }
-
-            File.WriteAllText(traitTrayScriptsPath, sb.ToString());
-        }
-
-        private void PatchAnimations()
-        {
-            string[] animationFilesToPatch = { Path.Combine(Game.Path, "Content/Game/Animations/Fx.sjson"), Path.Combine(Game.Path, "Content/Game/Animations/GUIAnimations.sjson"), Path.Combine(Game.Path, "Content/Game/Animations/ObstacleAnimations.sjson") };
-            foreach (var file in animationFilesToPatch)
-            {
-                var sb = new StringBuilder();
-                try
-                {
-                    using (StreamReader sr = new StreamReader(file))
-                    {
-                        string line;
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            string trimmedLine = line.Trim();
-                            if (trimmedLine.StartsWith("X = "))
-                            {
-                                line = ApplyUltrawideXOffsetForInteger(line);
-                            }
-                            else if (trimmedLine.StartsWith("OffsetX = "))
-                            {
-                                line = ApplyUltrawideXOffsetForInteger(line);
-                            }
-                            else if (trimmedLine.StartsWith("OriginX = "))
-                            {
-                                line = ApplyUltrawideXOffsetForInteger(line);
-                            }
-                            else if (trimmedLine.StartsWith("RandomOffsetX ="))
-                            {
-                                line = ApplyUltrawideXOffsetForDouble(line);
-                            }
-                            else if (trimmedLine.StartsWith("ScaleRadius ="))
-                            {
-                                line = ApplyUltrawideXOffsetForInteger(line);
-                            }
-                            else if (trimmedLine.StartsWith("RandomOffsetX ="))
-                            {
-                                line = ApplyUltrawideXOffsetForInteger(line);
-                            }
-                            else if (trimmedLine.StartsWith("ScaleRadius ="))
-                            {
-                                line = ApplyUltrawideXOffsetForDouble(line);
-                            }
-                            sb.AppendLine(line);
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"File {file} could not be read!");
-                    Console.WriteLine(e.ToString());
-                }
-                File.WriteAllText(file, sb.ToString());
-            }
-        }
-        private void PatchDebugKeyScreen()
-        {
-            var debugKeyScreenPath = Path.Combine(Game.Path, "Content/Game/GUI/DebugKeyScreen.sjson");
-            var sb = new StringBuilder();
-            try
-            {
-                using (StreamReader sr = new StreamReader(debugKeyScreenPath))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        string trimmedLine = line.Trim();
-                        if (trimmedLine.StartsWith("SpacingX = "))
-                        {
-                            line = ApplyUltrawideXOffsetForInteger(line);
-                        }
-                        sb.AppendLine(line);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"File {debugKeyScreenPath} could not be read!");
-                Console.WriteLine(e.ToString());
-            }
-            File.WriteAllText(debugKeyScreenPath, sb.ToString());
-        }
-        private void PatchAsphodel()
-        {
-            var filePath = Path.Combine(Game.Path, "Content/Game/Obstacles/Asphodel.sjson");
-            var sb = new StringBuilder();
-            try
-            {
-                using (StreamReader sr = new StreamReader(filePath))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        if (line.Contains("{ X = 960 Y = -244 }"))
-                        {
-                            line = line.Replace("960", NewCenterX.ToString());
-                        }
-                        sb.AppendLine(line);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"File {filePath} could not be read!");
-                Console.WriteLine(e.ToString());
-            }
-            File.WriteAllText(filePath, sb.ToString());
-        }
-
-        private void PatchObstacles()
-        {
-            var filePath = Path.Combine(Game.Path, "Content/Game/Obstacles/1_DevObstacles.sjson");
-            var sb = new StringBuilder();
-            try
-            {
-                using (StreamReader sr = new StreamReader(filePath))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        if (line.Contains("{ X = 1920") || line.Contains("{ X = -1920"))
-                        {
-                            line = line.Replace("1920", NewInternalWidth.ToString());
-                        }
-                        sb.AppendLine(line);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"File {filePath} could not be read!");
-                Console.WriteLine(e.ToString());
-            }
-            File.WriteAllText(filePath, sb.ToString());
-        }
         public void PatchCreateScreenObstacle()
         {
             var filePath = Path.Combine(Game.Path, "Content/Scripts/UIScripts.lua");
@@ -362,11 +103,33 @@ namespace HadesUltrawideGUIPatcher
                 using (StreamReader sr = new StreamReader(filePath))
                 {
                     string line;
+                    int i = 0;
                     while ((line = sr.ReadLine()) != null)
                     {
-                        if (line.Contains("local obstacleId = S"))
+                        i++;
+                        
+                        if (i == 2356)
                         {
-                            line = line.Replace("OffsetX = params.X", $"OffsetX = params.X + {NewCenterX - OriginalCenterX}");
+                                line = line.Insert(0, $@"
+--- trawzifieds Ultrawide Patch ---
+	local sideAlignmentThreshold = {SideAlignmentThreshold}
+	local originalInternalWidth = ScreenCenterX * 2
+	local newInternalWidth = {NewInternalWidth}
+	local newCenterX = {NewInternalWidth / 2}
+	local maxCenterPosition = originalInternalWidth - sideAlignmentThreshold
+	if params.Position == nil then
+		if params.X >= sideAlignmentThreshold and params.X <= maxCenterPosition then
+			params.X = params.X - ScreenCenterX + newCenterX
+		elseif params.X > originalInternalWidth - sideAlignmentThreshold then
+			params.X = newInternalWidth + params.X - originalInternalWidth
+		end
+	elseif params.Position == " + "\"" + "Center" + "\"" + @"then
+		params.X = params.X - ScreenCenterX + newCenterX
+	elseif params.Position == " + "\"" + "Right" + "\"" + @"then
+		params.X = newInternalWidth + params.X - originalInternalWidth
+	end
+	--- End Ultrawide Patch ---
+");
                         }
                         sb.AppendLine(line);
                     }
@@ -393,7 +156,7 @@ namespace HadesUltrawideGUIPatcher
                     {
                         if (line.Contains("ScreenAnchors.Vignette = CreateScreenObstacle"))
                         {
-                            line = line.Replace("Y = ScreenCenterY, ", $"Y = ScreenCenterY, Scale = {_screen.SixteenNineScaleFactor.ToString(CultureInfo.InvariantCulture)},");
+                            line = line.Replace("X = ScreenCenterX, Y = ScreenCenterY, ", $"X = ScreenCenterX, Y = ScreenCenterY, Scale = {_screen.SixteenNineScaleFactor.ToString(CultureInfo.InvariantCulture)}, Position = \"Center\", ");
                         }
                         sb.AppendLine(line);
                     }
@@ -407,33 +170,6 @@ namespace HadesUltrawideGUIPatcher
             File.WriteAllText(filePath, sb.ToString());
 
         }
-        private string ApplyUltrawideXOffsetForDouble(string line)
-        {
-            var splitSentence = line.Split('=');
-            double.TryParse(splitSentence[1], out double num);
-            if (num == OriginalCenterX)
-            {
-                splitSentence[1] = (NewCenterX + (num - OriginalCenterX)).ToString();
-                var newLine = string.Join("= ", splitSentence);
-                line = newLine;
-            }
-
-            return line;
-        }
-        private string ApplyUltrawideXOffsetForInteger(string line)
-        {
-            var splitSentence = line.Split('=');
-            int.TryParse(splitSentence[1], out int num);
-            if (num == OriginalCenterX)
-            {
-                splitSentence[1] = (NewCenterX + (num - OriginalCenterX)).ToString();
-                var newLine = string.Join("= ", splitSentence);
-                line = newLine;
-            }
-
-            return line;
-        }
-
         public SteamGame Game { get; }
 
         public const int OriginalInternalWidth = 1920;
